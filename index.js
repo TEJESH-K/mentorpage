44a12cf5629cc7a5013c4a29940db44759f9dbd3
unknown <k.tejesh2017@vitstudent.ac.in>
2021-03-15 15:19:04 +0530

    javascript changes

---
+++ index.js
@@ -0,0 +1,10 @@
+const name="Tejesh";
+const age=20;
+const address="mnpli";
+const x="my name is"+name+", age is "+age+",address is "+address;
+const z=`My name is ${name} and i am ${age}`;
+console.log(x);
+console.log(z);
+
+const y=x.split(",");
+console.log(y)
