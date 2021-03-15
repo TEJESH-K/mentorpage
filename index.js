b69d79bf7b727974383be6db76025f23afaf7736
unknown <k.tejesh2017@vitstudent.ac.in>
2021-03-15 21:23:03 +0530

    EXTRA PALINDROME

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ console.log(x);
 console.log(z);
 
 const y=x.split(",");
-console.log(y)*/
+console.log(y)
 
-const x="0";
+
+//palindrome
+
+
+const x="tet";
 const y=x.split('');
 //console.log(y);
 const z=y.reverse();
@@ -28,4 +32,140 @@ else{
     p="no, not paliandrome";
 }
 console.log(p);
+} 
+
+//arrays
+
+const fruits=['apples','oranges','pears'];
+fruits[3]='grapes';
+fruits.push('perry');
+fruits.unshift('strawberry'); //beginning
+fruits.pop();
+
+console.log(Array.isArray(fruits)); //check if the above one is array or not
+console.log(fruits.indexOf('oranges'));
+console.log(fruits);
+
+
+//object literals
+
+const person =
+{
+    fname:'tej',
+    lname:'k',
+    age : 30,
+    hobbies:['music','movies'],
+    address:{ street:'mnpli',city:'plnmr'}
 }
+person.email='tej@gmail.com'
+console.log(person);
+console.log(person.hobbies[1]);
+console.log(person.age);
+
+const {fname,age,hobbies,address:{city}}=person;
+console.log(age);
+console.log(hobbies);
+console.log(city);
+*/
+
+// using objects inside arrays
+
+const todos= 
+[
+    {
+        id:1,
+        text:'meeting with tej',
+        comp:true
+    },
+
+    {
+        id:2,
+        text:'meeting with doc',
+        comp:false
+    },
+
+    {
+        id:3,
+        text:'meeting with ram',
+        comp:true
+    }
+
+]
+console.log(todos);
+console.log(todos[1].id)
+console.log(todos[2].comp)
+
+//to JSON
+
+const todoJSON = JSON.stringify(todos);
+console.log(todoJSON);
+
+
+//FOR LOOP
+
+for(let i=0;i<5;i++)
+{
+    console.log(i);
+}
+
+
+//while
+i=0;
+while(i<5)
+{
+    console.log(i);
+    i++;
+}
+
+//FOR LOOP
+
+for(let i=0;i<todos.length;i++)
+{
+    console.log(todos[i].text);
+}
+
+//FOR-OF LOOP
+
+for(let t of todos)
+{
+    console.log(t.id);
+}
+//FOR-EACH
+
+todos.forEach(
+
+    function(t)
+{
+    console.log(t.text);
+}
+
+);
+
+//map
+
+const x=todos.map(function(t)
+{
+    return t.text;
+    return t.id;
+}
+);
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
