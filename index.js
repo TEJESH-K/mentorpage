8c5f202aacbc4e2fa5fa3beb56e34a2bf1a2c45a
unknown <k.tejesh2017@vitstudent.ac.in>
2021-03-15 16:54:29 +0530

    palindrome

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const name="Tejesh";
+/*const name="Tejesh";
 const age=20;
 const address="mnpli";
 const x="my name is"+name+", age is "+age+",address is "+address;
@@ -7,4 +7,25 @@ console.log(x);
 console.log(z);
 
 const y=x.split(",");
-console.log(y)
+console.log(y)*/
+
+const x="0";
+const y=x.split('');
+//console.log(y);
+const z=y.reverse();
+//console.log(z);
+let p;
+for(let i=0;i<y.length;i++)
+
+
+{
+    
+    if(x[i]==z[i])
+{
+     p="yes it is paliandrome";
+}
+else{
+    p="no, not paliandrome";
+}
+console.log(p);
+}
